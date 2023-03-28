5532332662a5713ac8c8328d91377b3d428262d1
Iwavvnc1 <popigbang@yandex.ru>
2023-03-28 18:05:17 +0500

    sprint9 new Validation

--- src/main/java/ru/yandex/practicum/filmorate/CustomValidator/ReleaseDateConstraint.java
+++ src/main/java/ru/yandex/practicum/filmorate/CustomValidator/ReleaseDateConstraint.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = ReleaseDateValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReleaseDateConstraint {
     String message() default "Invalid release date";
